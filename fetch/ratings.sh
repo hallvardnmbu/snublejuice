614391b79ee7cabac8610ce4bd091dfd6d314919
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2025-02-07 15:37:34 +0100

    Add bash script to run remotely.

---
+++ fetch/ratings.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source ~/.profile
+cd /home/snublejuice/Documents/snublejuice
+
+# 1. Connect to NordVPN and ensure connection is established
+echo "Connecting to NordVPN..."
+nordvpn connect || {
+    echo "Failed to connect to VPN";
+    exit 1
+}
+sleep 10
+if ! nordvpn status | grep -i "connected"; then
+    echo "Failed to connect to VPN";
+    exit 1
+fi
+
+# 2. Run the script
+echo "Running script..."
+bun run ./fetch/vivino/rating.mjs || {
+    echo "Failed to run fetch/vivino/rating.mjs";
+    nordvpn disconnect;
+    exit 1
+}
+
+# 3. Disconnect from NordVPN
+echo "Disconnecting from NordVPN..."
+nordvpn disconnect || {
+    echo "Failed to disconnect from VPN";
+    exit 1
+}
+
+echo "Script completed successfully"

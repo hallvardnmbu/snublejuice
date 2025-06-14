8146b64ead51c101463ada4a51fcf508c8c08ba7
Hallvard Høyland Lavik <hallvardlavik@gmail.com>
2025-06-14 17:46:58 +0200

    Update scripts somewhat.

--- fetch/scripts/monthly.sh
+++ fetch/scripts/monthly.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-LOG_FILE="/home/snublejuice/Documents/logs/monthly-$(date +'%Y-%m-%d_%H-%M-%S').log"
+# docker exec playground-javascript bash /home/snublejuice/fetch/scripts/monthly.sh
+
+LOG_FILE="$HOME/snublejuice/logs/monthly-$(date +'%Y-%m-%d_%H-%M-%S').log"
 
 log() {
     local message="$1"
@@ -18,36 +20,25 @@ abort() {
     exit 1
 }
 
-cd /home/snublejuice/Documents/snublejuice || abort "Failed to change directory to project root."
+cd "$HOME/snublejuice" || abort "Failed to change directory to project root."
 
 # Load environment variables from .env
 if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
-# Include bun executable
-export PATH=$PATH:/home/snublejuice/.bun/bin
-
-# 1. Connect to internet and ensure connection is established
-log "Connecting to internet"
-nordvpn connect || abort "Failed to connect to VPN."
-sleep 5
-if ! nordvpn status | grep -i "connected"; then
-    abort "Failed to establish VPN connection."
-fi
-
-# 2. Update prices
+# 1. Update prices
 log "Updating prices"
 bun run ./fetch/vinmonopolet/price.mjs 2>&1 | while IFS= read -r line; do log "$line" 0; done || abort "Failed to run fetch/vinmonopolet/price.mjs"
 bun run ./fetch/taxfree/price.mjs 2>&1 | while IFS= read -r line; do log "$line" 0; done || abort "Failed to run fetch/taxfree/price.mjs"
 
-# 3. Update stock
+# 2. Update stock
 log "Updating stock"
 bun run ./fetch/vinmonopolet/detailed.mjs 2>&1 | while IFS= read -r line; do log "$line" 0; done || abort "Failed to run fetch/vinmonopolet/detailed.mjs."
 bun run ./fetch/vinmonopolet/popular.mjs 2>&1 | while IFS= read -r line; do log "$line" 0; done || abort "Failed to run fetch/vinmonopolet/popular.mjs."
 bun run ./fetch/taxfree/stock.mjs 2>&1 | while IFS= read -r line; do log "$line" 0; done || abort "Failed to run fetch/taxfree/stock.mjs."
 
-# 4. Send mails to subscribers
+# 3. Send mails to subscribers
 log "Mailing subscribers"
 bun run ./fetch/email.mjs 2>&1 | while IFS= read -r line; do log "$line" 0; done || abort "Failed to run fetch/email.mjs"
 
--- fetch/scripts/ratings.sh
+++ fetch/scripts/ratings.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-LOG_FILE="/home/snublejuice/Documents/logs/ratings-$(date +'%Y-%m-%d_%H-%M-%S').log"
+# docker exec playground-javascript bash /home/snublejuice/fetch/scripts/ratings.sh
+
+LOG_FILE="$HOME/snublejuice/logs/ratings-$(date +'%Y-%m-%d_%H-%M-%S').log"
 
 log() {
     local message="$1"
@@ -18,25 +20,14 @@ abort() {
     exit 1
 }
 
-cd /home/snublejuice/Documents/snublejuice || abort "Failed to change directory to project root."
+cd "$HOME/snublejuice" || abort "Failed to change directory to project root."
 
 # Load environment variables from .env
 if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
-# Include bun executable
-export PATH=$PATH:/home/snublejuice/.bun/bin
-
-# 1. Connect to internet and ensure connection is established
-log "Connecting to internet"
-nordvpn connect || abort "Failed to connect to VPN."
-sleep 5
-if ! nordvpn status | grep -i "connected"; then
-    abort "Failed to establish VPN connection."
-fi
-
-# 2. Run the script
+# 1. Run the script
 log "Updating ratings"
 bun run ./fetch/vivino/rating.mjs 2>&1 | while IFS= read -r line; do log "$line" 0; done || abort "Failed to update ratings"
 
